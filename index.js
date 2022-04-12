9f7da0ead77648d025a9fac42627e031794d65a0
Ye Yangchen <yye02@email.wm.edu>
2022-04-12 06:25:36 -0400

    fix script template

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import core from '@actions/core'
 import fetch from 'node-fetch';
-import captureWebsite from 'capture-website';
 
 const user_info_url = 'https://api.bilibili.com/x/web-interface/card'
 const page_url = 'https://space.bilibili.com'
@@ -16,22 +14,9 @@ const fetch_follower = async () => {
 	return content.data.follower;
 };
 
-const screenshot = async (dirpath, follower) => {
-	const filename = `${dirpath}/${follower}.png`
-
-	await captureWebsite.file(`${page_url}/${mid}`, filename)
-}
-
 const run = async() => {
 	let follower = await fetch_follower()
 	console.log(follower)
-	// if (Math.abs(follower - target) < 5) {
-	// 	try {
-	// 		await screenshot('pic', follower)
-	// 	} catch {
-	// 		// pass
-	// 	}
-	// }
 }
 
 run()

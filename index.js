ba85811cfec86b4731598b011c6e9d269dc6fe64
Ye Yangchen <yye02@email.wm.edu>
2022-04-12 06:46:58 -0400

    use secrets variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
+import core from '@actions/core'
 
-const user_info_url = 'https://api.bilibili.com/x/web-interface/card'
-const page_url = 'https://space.bilibili.com'
-const mid = '25415856'
-const target = 1789
+const user_info_url = core.getInput('api_url', { required: true })
+const page_url = core.getInput('page_url', { required: true })
+const mid = core.getInput('mid', { required: true })
+const target = core.getInput('target', { required: true })
 
 const fetch_follower = async () => {
 	const response = await fetch(`${user_info_url}?mid=${mid}`);
